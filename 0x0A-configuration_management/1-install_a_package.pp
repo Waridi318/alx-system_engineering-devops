c4890966488baefed5e7699ca53ba5c32c235b10
Waridi318 <rosenjeri7151@gmail.com>
2023-06-23 13:29:17 +0300

    Task 1 using exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
 # this manifest installs flask
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/bin',
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  refreshonly => true,
-}
-
-exec { 'install_werkzeug':
-  command     => '/usr/bin/pip3 install werkzeug==2.1.1',
-  path        => '/usr/bin',
-  unless      => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
-  refreshonly => true,
+exec {'flask':
+  command => 'usr/bin/pip3 install flask -v 2.1.0',
 }
