3afc86641ad9e0c2ac7014e4dfa86b46f1506fe0
Waridi318 <rosenjeri7151@gmail.com>
2023-06-23 13:50:11 +0300

    Updated werkzeug version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # this manifest installs flask
 
-exec {'flask':
-  command => 'usr/bin/pip3 install flask -v 2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
