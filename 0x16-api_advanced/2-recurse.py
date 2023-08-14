e9501cc3fc8791e91c4474d017e9d428a232ea6d
Waridi318 <rosenjeri7151@gmail.com>
2023-08-14 17:07:03 +0300

    Added limit and count url parameters

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,14 +3,16 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     params = {
-        "after": after
+        "after": after,
+        "count": count,
+        "limit": 100
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
@@ -19,10 +21,11 @@ def recurse(subreddit, hot_list=[], after=""):
 
     results = response.json().get("data")
     after = results.get("after")
+    count += results.get("children")
 
     for c in results.get("children"):
         hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-        return recurse(subreddit, hot_list, after)
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
