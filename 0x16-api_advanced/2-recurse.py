2be21e4dba1ba74a505269c6bd7698e8d7580534
Waridi318 <rosenjeri7151@gmail.com>
2023-08-14 17:02:38 +0300

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
-"""
-This module defines a function that queries the Reddit API
-and returns a lit containing the titles of all hot articles
-for a given subreddit
-"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
+import requests
 
 
-from requests import get
-
-
-def recurse(subreddit, hot_list=[], page=None):
-    """
-    queries the Reddit API
-    and returns a list containing the titles of all hot articles
-    for a given subreddit
-    """
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16-api_advanced:v1.0.0"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
-    if page:
-        url += f"&after={page}"
-    response = get(url, headers=headers, allow_redirects=False)
-
+    params = {
+        "after": after
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
-        return (None)
-    data = response.json()
-    results = data.get("data").get("children")
-    for post in results:
-        title = post["data"]["title"]
-        hot_list.append(title)
-#        print(title)
+        return None
 
-    page = data.get("data").get("after")
-    if page is not None:
-        return(recurse, hot_list, page)
-    else:
-        return(None)
+    results = response.json().get("data")
+    after = results.get("after")
 
-    return(hot_list)
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
